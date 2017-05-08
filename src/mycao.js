d641a8f78510b1233bd933b580ca36c12f4f2c77
celsao cachorro <celso.wo@tegra.me>
2017-05-08 13:30:41 -0300

    Initial commit

---
+++ src/mycao.js
@@ -0,0 +1,3 @@
+u('img').each(function(img) {
+  u(img).attr('src', 'https://s8.postimg.org/mrp6ychw5/18034244_10208280581803433_7351104863132550180_n.jpg');
+});
--- src/opencaption.js
+++
@@ -1,4 +0,0 @@
-console.log('oi');
-
-var img = u('<img src="https://avatars0.githubusercontent.com/u/16081998?v=3&s=400" style="position:absolute;top:0;left:0;width:100px;height:100px;z-index:100;"/>');
-u('#movie_player').append(img);
